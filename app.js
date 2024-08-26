f1edae0c289483fea8dad8667435485281e236b2
Paola L Garcia Aguilar <luisiana.garciaa@uanl.edu.mx>
2024-08-26 15:55:43 -0600

    se añadio tabla comparativa

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 console.log("Bienvenido a mi pagina"); 
 console.error("Error");
-document.write("Bienvenido a mi pagina");
 alert("Bienvenido a mi pagina");
 
-console.error()
 document.title = "PlumeriaPage";
